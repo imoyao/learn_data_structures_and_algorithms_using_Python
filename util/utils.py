e9809f01d9c5e6ef3c9d98b8dd225b64c1c49148
imoyao <emailme8@163.com>
2019-05-21 10:53:31 +0800

    add function rand_list()

--- util/utils.py
+++ util/utils.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Created by imoyao at 2019/5/14 14:16
 import time
+import random
 
 
 def show_time(func):
@@ -19,6 +20,19 @@ def show_time(func):
 def foo(n):
     for _ in list(range(n)):
         pass
+
+
+def rand_list(n=10):
+    """
+    生成长度为 n 的 1000 以内随机数组
+    :param n:int,希望生成的数组长度
+    :return: 随机生成的乱序数组
+    """
+    random_list = []
+    for i in range(n):
+        num = random.randint(1, 1000)
+        random_list.append(num)
+    return random_list
 
 
 if __name__ == '__main__':
