2a8d1f322e086d8f6c5536ec3e7fc797dc1e5fc5
imoyao <emailme8@163.com>
2019-11-20 10:00:46 +0800

    tree code

--- Tree/Binary_tree/bintree_bfs_and_dfs.py
+++ Tree/Binary_tree/bintree_bfs_and_dfs.py
@@ -111,10 +111,108 @@ class BinTree:
             print(node.data, end=' ')
 
 
+class BinTreeStack:
+    """
+    使用栈模拟二叉树
+    栈：后进先出（LIFO）
+    """
+
+    def __init__(self, root=None):
+        # super().__init__(root)  # python3
+        # super(BinTreeStack, self).__init__(root)  # python2
+        self.root = root
+        self.stack = []
+
+    def make_tree(self, tree, val):
+        if val is None:
+            return
+        else:
+            tree.data = val
+            tree.left_subtree = Node(val)
+            self.make_tree(tree.left_subtree, val)
+            tree.right_subtree = Node(val)
+            self.make_tree(tree.right_subtree, val)
+
+    def dfs_travel_stack_preorder(self, tree):
+        """
+        栈的方式前序遍历
+        先把根节点入栈，然后左节点，最后右节点
+        :return:
+        """
+        if self.root is None:
+            return
+        stack = []
+        node = tree
+        while node or stack:
+            while node:
+                print(node.data)
+                stack.append(node)
+                node = node.left_subtree
+            node = stack.pop()
+            node = node.right_subtree
+
+    def dfs_travel_stack_inorder(self, tree):
+        """
+        以栈的方式中序遍历 左 -> 根 -> 右
+        :param tree:
+        :return:
+        """
+        if self.root is None:
+            return
+        stack = []
+        node = tree
+        while node or stack:
+            stack.append(node)
+            node = node.left_subtree
+        node = stack.pop()
+        print(node.data)
+        node = node.right_subtree
+
+    def dfs_travel_stack_postorder(self, tree):
+        if self.root is None:
+            return
+        stack_1 = []
+        stack_2 = []
+        node = tree
+        stack_1.append(node)
+        while stack_1:
+            node = stack_1.pop()
+            if node.left_subtree:
+                stack_1.append(node.left_subtree)
+            if node.right_subtree:
+                stack_1.append(node.right_subtree)
+            stack_2.append(node)
+        while stack_2:
+            node = stack_2.pop()
+            print(node.data)
+
+
 if __name__ == '__main__':
     t = BinTree()
     for i in range(10):
-        t.add_node(1)
+        t.add_node(i)
+
+    print('bfs_travel:')
     t.bfs_travel()
-    print('bfs_travel')
-    t.dfs_travel_preorder()
+    print('\ndfs_travel_preorder:')
+    t.dfs_travel_preorder(t.root)
+    print('\ndfs_travel_inorder:')
+    t.dfs_travel_inorder(t.root)
+    print('\ndfs_travel_postorder:')
+    t.dfs_travel_postorder(t.root)
+    '''
+    bfs_travel:
+    0 1 2 3 4 5 6 7 8 9 
+    dfs_travel_preorder:
+    0 1 3 7 8 4 9 2 5 6 
+    dfs_travel_inorder:
+    7 3 8 1 9 4 0 5 2 6 
+    dfs_travel_postorder:
+    7 8 3 9 4 1 5 6 2 0 
+    '''
+    print('*' * 20, end='\n')
+    # st = BinTreeStack()
+    # for i in range(10):
+    #     st.make_tree(st, i)
+    # st.dfs_travel_stack_preorder(st)
+    # # st.dfs_travel_stack_inorder(st.root)
